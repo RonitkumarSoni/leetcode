2ff9943cce089ed45bcf376dc7f817a4e727ab52
Ronit Soni <ronitkumarsoni.cg@gmail.com>
2026-04-11 10:11:28 +0000

    Sync LeetCode submission Runtime - 1 ms (57.33%), Memory - 8.7 MB (23.14%)

---
+++ Leetcode-Solutions/0009-palindrome-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        int input = x;
+        long reverse = 0;
+
+        while(x>0){
+            int digit = x%10;
+            reverse = reverse *10 + digit;
+            x = x/10;
+        }
+        return input == reverse;
+    }
+};
