03bef1c7f63c72912a38e1dcb1ab4c4d87ea7860
Ronit Soni <ronitkumarsoni.cg@gmail.com>
2026-04-21 05:54:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (87.66%)

---
+++ Leetcode-Solutions/0767-prime-number-of-set-bits-in-binary-representation/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        return n == 2 || n == 3 || n == 5 || n == 7 || 
+               n == 11 || n == 13 || n == 17 || n == 19;
+    }
+
+    int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (isPrime(__builtin_popcount(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
