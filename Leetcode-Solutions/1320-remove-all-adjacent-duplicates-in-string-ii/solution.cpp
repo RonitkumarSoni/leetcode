69ced2a0b1fa44a44b537cddc45515df2aa59565
Ronit Soni <ronitkumarsoni.cg@gmail.com>
2026-06-06 08:21:49 +0000

    Sync LeetCode submission Runtime - 4 ms (76.62%), Memory - 14 MB (59.96%)

---
+++ Leetcode-Solutions/1320-remove-all-adjacent-duplicates-in-string-ii/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char,int>> st;
+
+        for(char ch : s) {
+
+            if(!st.empty() && st.top().first == ch) {
+                st.top().second++;
+
+                if(st.top().second == k) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push({ch,1});
+            }
+        }
+
+        string ans = "";
+
+        while(!st.empty()) {
+            char ch = st.top().first;
+            int freq = st.top().second;
+
+            while(freq--) {
+                ans += ch;
+            }
+
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
